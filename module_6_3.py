6f830348a6b6e307b0b9dec690f816413cd0e60c
JPyanova <j.pyanova1994@gmail.com>
2024-10-15 19:35:31 +0700

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,45 +1,50 @@
 class Horse:
+    x_distance = 0
+    sound = 'Frrr'
 
     def __init__(self, x_distance, sound):
-        self.x_distance = 0
-        self.sound = 'Frrr'
-        super().__init__()
+        self.x_distance = x_distance
+        self.sound = sound
+        super().__init__(y_distance)
 
     def run(self, dx):
-        dx = self.x_distance + dx
+        dx += self.x_distance
 
 class Eagle:
 
+    y_distance = 0
+    sound = 'I train, eat, sleep and repeat'
+
     def __init__(self, y_distance, sound):
-        self.y_distance = 0
-        self.sound = 'I train, eat, sleep and repeat'
-        super().__init__()
+        self.y_distance = y_distance
+        self.sound = sound
+        super().__init__(x_distance)
 
     def fly(self, dy):
-        dy = self.y_distance + dy
+        dy += self.y_distance
 
 class Pegasus (Horse, Eagle):
 
-    def __init__(self, x_distance, y_distance, sound):
-        super().__init__(x_distance)
-        super().__init__(y_distance, sound)
+    def __init__(self):
+        super().__init__()
+        super().__init__()
 
     def move(self, dx, dy):
         super().run(dx)
         super().fly(dy)
 
     def get_pos(self):
-        position = [self.x_distance, self.y_distance]
+        return super().x_distance, super().y_distance
 
     def voice(self):
-        inherited_voice = self.voice
+        return super().sound
 
 p1 = Pegasus()
 
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
-p1.move(-5, 20)
+p1.move(-5,20)
 print(p1.get_pos())
 
 p1.voice()
